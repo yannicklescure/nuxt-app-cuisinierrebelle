1629b926782e01726876c26c5f6d7f8c70e57306
yannick lescure <yannick.lescure@gmail.com>
2021-02-25 16:06:31 -0500

    first commit

---
+++ nuxt.config.js
@@ -0,0 +1,53 @@
+export default {
+  // Target: https://go.nuxtjs.dev/config-target
+  target: 'static',
+
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'nuxt-app-cuisinierrebelle',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+  ],
+
+  publicRuntimeConfig: {
+    axios: {
+      baseURL: 'https://api.cuisinierrebelle.com'
+    }
+  },
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  // Modules - https://nuxtjs.org/docs/2.x/directory-structure/modules
+  modules: [
+    '@nuxtjs/axios',
+    // https://go.nuxtjs.dev/bootstrap
+    'bootstrap-vue/nuxt',
+  ],
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+  }
+}
