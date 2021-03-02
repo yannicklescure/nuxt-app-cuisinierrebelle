7bbe4157a7c71ddaf22013c737a9621cd65c4eeb
yannick lescure <yannick.lescure@gmail.com>
2021-03-02 14:05:38 -0500

    improve code + fix bugs

--- components/Recipe/Body.vue
+++ components/Recipe/Body.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div class="d-none d-print-block mt-3 mb-5 text-center">∾&nbsp;www.CuisinierRebelle.com&nbsp;∾</div>
-    <div class="mt-md-5 order-1 order-md-1 d-flex flex-column justify-content-center align-items-center">
+    <div class="my-md-5 order-1 order-md-1 d-flex flex-column justify-content-center align-items-center">
       <div class="text-center">
         <div class="h1">{{ item.recipe.title }}</div>
         <div v-if="item.recipe.subtitle" class="h2 text-secondary">{{ item.recipe.subtitle }}</div>
--- pages/r/_slug.vue
+++ pages/r/_slug.vue
@@ -56,31 +56,31 @@ export default {
       return this.recipe(this.slug)
     },
   },
-  // metaInfo () {
-  //   console.log(this.item)
-  //   if (this.item != undefined) {
-  //     return {
-  //       title: this.item.recipe.title,
-  //       meta: [
-  //         { vmid: 'description', name: 'description', content: this.item.recipe.description },
-  //         { vmid: 'fb:app_id', property: 'fb:app_id', content: '570259036897585' },
-  //         { vmid: 'og:site_name', property: 'og:site_name', content: 'Cuisinier Rebelle' },
-  //         { vmid: 'og:title', property: 'og:title', content: `${ this.item.recipe.title } | Cuisinier Rebelle` },
-  //         { vmid: 'og:description', property: 'og:description', content: this.item.recipe.description },
-  //         { vmid: 'og:locale', property: 'og:locale', content: 'fr_FR' },
-  //         { vmid: 'og:type', property: 'og:type', content: 'website' },
-  //         { vmid: 'og:url', property: 'og:url', content: `https://www.cuisinierrebelle.com/r/${ this.$route.params.slug }` },
-  //         { vmid: 'og:image', property: 'og:image', content: this.item.recipe.photo.openGraph.url },
-  //         { vmid: 'twitter:card', name: 'twitter:card', content: 'summary_large_image' },
-  //         { vmid: 'twitter:site', name: 'twitter:site', content: '@cuisinierrebelle' },
-  //         { vmid: 'twitter:creator', name: 'twitter:creator', content: '@cuisinierrebelle' },
-  //         { vmid: 'twitter:title', name: 'twitter:title', content: `${ this.item.recipe.title } | Cuisinier Rebelle` },
-  //         { vmid: 'twitter:description', name: 'twitter:description', content: this.item.recipe.description },
-  //         { vmid: 'twitter:image', name: 'twitter:image', content: this.item.recipe.photo.openGraph.url },
-  //       ]
-  //     }
-  //   }
-  // },
+  metaInfo () {
+    console.log(this.item)
+    if (this.item != undefined) {
+      return {
+        title: this.item.recipe.title,
+        meta: [
+          { vmid: 'description', name: 'description', content: this.item.recipe.description },
+          { vmid: 'fb:app_id', property: 'fb:app_id', content: '570259036897585' },
+          { vmid: 'og:site_name', property: 'og:site_name', content: 'Cuisinier Rebelle' },
+          { vmid: 'og:title', property: 'og:title', content: `${ this.item.recipe.title } | Cuisinier Rebelle` },
+          { vmid: 'og:description', property: 'og:description', content: this.item.recipe.description },
+          { vmid: 'og:locale', property: 'og:locale', content: 'fr_FR' },
+          { vmid: 'og:type', property: 'og:type', content: 'website' },
+          { vmid: 'og:url', property: 'og:url', content: `https://www.cuisinierrebelle.com/r/${ this.$route.params.slug }` },
+          { vmid: 'og:image', property: 'og:image', content: this.item.recipe.photo.openGraph.url },
+          { vmid: 'twitter:card', name: 'twitter:card', content: 'summary_large_image' },
+          { vmid: 'twitter:site', name: 'twitter:site', content: '@cuisinierrebelle' },
+          { vmid: 'twitter:creator', name: 'twitter:creator', content: '@cuisinierrebelle' },
+          { vmid: 'twitter:title', name: 'twitter:title', content: `${ this.item.recipe.title } | Cuisinier Rebelle` },
+          { vmid: 'twitter:description', name: 'twitter:description', content: this.item.recipe.description },
+          { vmid: 'twitter:image', name: 'twitter:image', content: this.item.recipe.photo.openGraph.url },
+        ]
+      }
+    }
+  },
   created () {
     if (this.item == undefined) {
       // console.log(this.item)
