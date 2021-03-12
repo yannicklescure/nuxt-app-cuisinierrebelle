41dcb2373891741bd8889181f290116b0b91e221
yannick lescure <yannick.lescure@gmail.com>
2021-03-12 18:30:38 -0500

    improve code + fix bugs

--- nuxt.config.js
+++ nuxt.config.js
@@ -264,26 +264,9 @@ export default {
     terser: {
       // https://github.com/terser/terser#compress-options
       terserOptions: {
-        parse: {
-        // Let terser parse ecma 8 code but always output
-        // ES5 compliant code for older browsers
-          // ecma: 8
-        },
         compress: {
-          // ecma: 5,
-          ecma: 8,
-          warnings: false,
-          comparisons: false,
           drop_console: true
         },
-        mangle: {
-          safari10: true
-        },
-        output: {
-          // ecma: 5,
-          // comments: false,
-          ascii_only: true
-        }
       },
     },
   },
