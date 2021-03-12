46db701c1a4964675e53238f86bd1590bf378eb8
yannick lescure <yannick.lescure@gmail.com>
2021-03-12 18:14:30 -0500

    improve code + fix bugs

--- nuxt.config.js
+++ nuxt.config.js
@@ -244,6 +244,23 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+    postcss: {
+      // Add plugin names as key and arguments as value
+      // Install them before as dependencies with npm or yarn
+      plugins: {
+        // Disable a plugin by passing false as value
+        'postcss-url': false,
+        // 'postcss-nested': {},
+        'postcss-responsive-type': {},
+        'postcss-hexrgba': {}
+      },
+      preset: {
+        // Change the postcss-preset-env settings
+        autoprefixer: {
+          grid: true
+        }
+      }
+    },
     terser: {
       // https://github.com/terser/terser#compress-options
       terserOptions: {
