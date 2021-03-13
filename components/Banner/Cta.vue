115366c20b700e6a7236dcd177d39b8a1255f4ff
yannick lescure <yannick.lescure@gmail.com>
2021-03-12 20:56:59 -0500

    improve code + fix bugs

--- components/Banner/Cta.vue
+++ components/Banner/Cta.vue
@@ -2,12 +2,12 @@
   <div class="d-flex flex-column flex-grow-1 justify-content-center align-items-center">
     <div class="container">
       <div id="banner-cta-box" class="text-center">
-        <div class="h4 text-white text-center" v-html="$t('banner.introduction')"></div>
+        <div class="h2 text-white text-center" v-html="$t('banner.introduction')"></div>
       </div>
       <div id="banner-cta-box-btn" class="d-flex mt-3 justify-content-center flex-column flex-md-row">
         <NuxtLink
           to="/signup"
-          class="btn btn-info mt-3 mx-3 mt-md-0"
+          class="btn btn-lg btn-info mt-3 mx-3 mt-md-0"
         >{{ $t('banner.getStarted') }}</NuxtLink>
       </div>
     </div>
