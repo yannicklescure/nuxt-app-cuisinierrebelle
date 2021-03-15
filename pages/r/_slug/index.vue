6c63b1fef60361f13e7d0b6a741575dbb176e99d
Yannick Lescure <yannick.lescure@gmail.com>
2021-03-15 18:11:12 -0400

    improve code + fix bugs

--- pages/r/_slug/index.vue
+++ pages/r/_slug/index.vue
@@ -1,19 +1,18 @@
 <template>
   <div ref="recipe" class="container py-3 mb-5 recipe" :key="componentKey">
-    <div v-if="item">
-      <RecipeHead :item="item" />
-      <RecipeBody :item="item" :dimension="dimension" />
+    <SocialHead :item="item" />
+    <RecipeHead :item="item" />
+    <RecipeBody :item="item" :dimension="dimension" />
 
-      <LazyYoutube :item="item" />
+    <LazyYoutube :item="item" />
 
-      <LazyBtnSocialSharing v-if="isMobile == false" :item="item" />
+    <LazyBtnSocialSharing v-if="isMobile == false" :item="item" />
 
-      <LazyRecipeAds />
+    <LazyRecipeAds />
 
-      <LazyOtherRecipes />
+    <LazyOtherRecipes />
 
-      <LazyComments :item="item" />
-    </div>
+    <LazyComments :item="item" />
   </div>
 </template>
 
@@ -42,64 +41,6 @@ export default {
     console.log(this.$route.params.slug)
     if (this.items.length < 2) this.getStoreData()
     // await this.fetchRecipe(this.$route.params.slug)
-  },
-  head() {
-    if (this.item) {
-      return {
-        meta: [
-          {
-            hid: 'twitter:title',
-            name: 'twitter:title',
-            content: this.item.recipe.title
-          },
-          {
-            hid: 'twitter:description',
-            name: 'twitter:description',
-            content: this.item.recipe.description
-          },
-          {
-            hid: 'twitter:image',
-            name: 'twitter:image',
-            content: this.item.recipe.photo.openGraph.url
-          },
-          {
-            hid: 'twitter:image:alt',
-            name: 'twitter:image:alt',
-            content: this.item.recipe.title
-          },
-          {
-            hid: 'fb:app_id',
-            property: 'fb:app_id',
-            content: '570259036897585'
-          },
-          {
-            hid: 'og:title',
-            property: 'og:title',
-            content: this.item.recipe.title
-          },
-          {
-            hid: 'og:description',
-            property: 'og:description',
-            content: this.item.recipe.description
-          },
-          {
-            hid: 'og:image',
-            property: 'og:image',
-            content: this.item.recipe.photo.openGraph.url
-          },
-          {
-            hid: 'og:image:secure_url',
-            property: 'og:image:secure_url',
-            content: this.item.recipe.photo.openGraph.url
-          },
-          {
-            hid: 'og:image:alt',
-            property: 'og:image:alt',
-            content: this.item.recipe.title
-          }
-        ]
-      }
-    }
   },
   computed: {
     ...mapGetters({
