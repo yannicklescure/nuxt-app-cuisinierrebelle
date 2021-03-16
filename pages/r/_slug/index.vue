265d1447096b7216da2da41c7030655e5e49c7f5
yannick lescure <yannick.lescure@gmail.com>
2021-03-15 20:41:34 -0400

    improve code + fix bugs

--- pages/r/_slug/index.vue
+++ pages/r/_slug/index.vue
@@ -37,41 +37,42 @@ export default {
       // item: undefined
     }
   },
-  async asyncData({ $axios, params }) {
-    // console.log(context)
-    // await context.store.dispatch('recipes/recipe', context.params.slug)
-    const item = await $axios.$get(`https://api.cuisinierrebelle.com/v1/recipes/${ params.slug }`)
-    return { item }
-  },
-  async fetch () {
-    console.log(this.$route.params.slug)
-    // this.item = await this.$axios.$get(`https://api.cuisinierrebelle.com/v1/recipes/${ this.$route.params.slug }`)
-    // console.log(this.item)
-    this.$store.commit("recipes/recipe", { data: this.item })
-    // await this.fetchRecipe(this.$route.params.slug)
-  },
+  // async asyncData({ $axios, params }) {
+  //   // console.log(context)
+  //   // await context.store.dispatch('recipes/recipe', context.params.slug)
+  //   const item = await $axios.$get(`https://api.cuisinierrebelle.com/v1/recipes/${ params.slug }`)
+  //   return { item }
+  // },
+  // async fetch () {
+  //   console.log(this.$route.params.slug)
+  //   // this.item = await this.$axios.$get(`https://api.cuisinierrebelle.com/v1/recipes/${ this.$route.params.slug }`)
+  //   // console.log(this.item)
+  //   this.$store.commit("recipes/recipe", { data: this.item })
+  //   // await this.fetchRecipe(this.$route.params.slug)
+  // },
   computed: {
     ...mapGetters({
       currentUser: 'users/sessions/current',
       isMobile: 'isMobile',
-      // recipe: 'recipes/recipe',
+      recipe: 'recipes/recipe',
       items: 'recipes/listSorted',
     }),
-    // item () {
-    //   return this.recipe(this.$route.params.slug)
-    // }
+    item () {
+      return this.recipe(this.$route.params.slug)
+    }
   },
   methods: {
     ...mapActions({
       getStoreData: 'getStoreData',
-      // fetchRecipe: 'recipes/recipe',
+      fetchRecipe: 'recipes/recipe',
     }),
     matchInfoBox () {
       this.dimension.width = this.$refs.recipe.clientWidth
       this.dimension.height = parseInt(this.dimension.width * 2 / 3)
     }
   },
-  created () {
+  async created () {
+    await this.fetchRecipe(this.$route.params.slug)
     if (this.items.length < 2) this.getStoreData()
     if (process.client) {
       window.addEventListener("resize", this.matchInfoBox);
