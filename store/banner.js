d9e37faee50687436dfbb8955142e069afe67395
yannick lescure <yannick.lescure@gmail.com>
2021-03-17 10:42:18 -0400

    improve code + fix bugs

---
+++ store/banner.js
@@ -0,0 +1,38 @@
+import * as api from '~/api'
+
+export const state = () => ({
+  image: {
+    id: null,
+    link: {
+      download: null,
+    },
+    url: null,
+    user: {
+      name: null,
+      username: null
+    }
+  },
+})
+
+export const mutations = {
+  set (state, payload) {
+    // console.log(payload)
+    state.image = payload.data.bannerImage
+  },
+}
+
+export const actions = {
+  async get ({ commit }, payload) {
+    console.log(this)
+    const response = await this.$axios.$get('https://api.cuisinierrebelle.com/v1/unsplash_images')
+    console.log(response)
+    this.commit('banner/set', response)
+    return response
+  },
+}
+
+export const getters = {
+  image (state) {
+    return state.image
+  },
+}
