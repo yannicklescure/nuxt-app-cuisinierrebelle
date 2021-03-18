351a5e31630b71a607d4ce8a7c70623e93ac9bbe
yannick lescure <yannick.lescure@gmail.com>
2021-03-18 19:33:50 -0400

    improve code + fix bugs

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -1,24 +1,27 @@
 <template>
   <div class="container">
-    <div class="h1 text-capitalize">admin</div>
-    <NuxtLink to="/admin/pages">Gestion des pages</NuxtLink>
-    <p v-if="theUser.name != null">{{ theUser.name }}</p>
+    <div class="h1 text-capitalize">{{ title }}</div>
+    <p>current logged in user: {{ currentUser.email }}</p>
+    <div v-if="currentUser.admin">
+      <NuxtLink to="/admin/pages">Gestion des pages</NuxtLink>
+    </div>
   </div>
 </template>
+
 <script>
+import { mapGetters, mapActions } from 'vuex'
+
 export default {
   name: 'Admin',
-  data() {
-    return {
-      theUser: {
-        name: null
-      }
-    }
-  },
   middleware: ['authenticated', 'admin'],
-  async fetch() {
-    const user = await this.$axios.$get(`https://api.cuisinierrebelle.com/v1/users/yannicklescure`)
-    this.theUser = user.data
+  computed: {
+    ...mapGetters({
+      currentUser: 'users/sessions/current',
+    }),
+    title () {
+      if (this.currentUser.admin) return 'admin dashboard'
+      if (this.currentUser.moderator) return 'moderator dashboard'
+    }
   },
 }
 </script>
