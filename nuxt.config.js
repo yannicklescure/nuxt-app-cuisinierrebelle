1eac45ec1234a810ef77f7ca2b4144a1019646fc
yannick lescure <yannick.lescure@gmail.com>
2021-04-02 22:20:59 -0400

    improve code + fix bugs

--- nuxt.config.js
+++ nuxt.config.js
@@ -261,7 +261,7 @@ export default {
 
   env: {
     baseUrl: process.env.BASE_URL || 'https://www.cuisinierrebelle.com',
-    // apiUrl: 'https://api.cuisinierrebelle.com'
-    apiUrl: 'http://localhost:5000'
+    apiUrl: 'https://api.cuisinierrebelle.com'
+    // apiUrl: 'http://localhost:5000'
   }
 }
